5c2aa99c72b6038f95678ec5568633a88e7179d3
Angelica Lazaro <angelicamld@gmail.com>
2025-06-24 00:10:24 +0200

    first configuration for frontend deploy

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,12 +1,19 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = [
+  "strapi::errors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://somosindigo.vercel.app/", "http://localhost:3000"],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      headers: "*",
+    },
+  },
+  "strapi::security",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
